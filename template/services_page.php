1c6f5d7eb6252c529864e1e17e734f9715057edd
shohel931 <shohel065931@gmail.com>
2025-03-30 16:26:25 +0600

    jhkjhkjhkjh

--- template/services_page.php
+++ template/services_page.php
@@ -95,31 +95,56 @@ get_header(); ?>
 
 
 
-
-
-
-
-
-
-
-  <div class="main_area">
-    <div class="sub_area">
-
-    </div>
-    <div class="image_area">
-    </div>
-    <div class="image_area1">
-    
-    </div>
-    <div class="image_area2">
-    
+<section id="section_3_1">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <h3><?php echo get_field('section_3_title_');?></h3>
+        <p><?php echo get_field('section_3_title_description');?></p>
+      </div>
+      <div class="col-md-6">
+        <div class="box1">
+          <h2><?php echo get_field('section_3_box_1_title');?></h2>
+          <p><?php echo get_field('section_3_box_1_title_description');?></p>
+        </div>
+        <div class="box1">
+          <h2><?php echo get_field('section_3_box_2_title');?></h2>
+          <p><?php echo get_field('section_3_box_2_title_description_');?></p>
+        </div>
+      </div>
     </div>
   </div>
+</section>
 
 
 
 
+<section id="section_4_1">
+  <div class="title_area1">
+    <h3><?php echo get_field('section_4_title'); ?></h3>
+  </div>
 
+  <div class="main_box1">
+    <div class="sub_box1">
+    <?php $image = get_field('section_4_box_1_image');?>
+    <img src="<?php echo $image;?>" alt="">
+      <h4><?php echo get_field('section_4_box_1_title'); ?></h4>
+      <p><?php echo get_field('section_4_box_1_description'); ?></p>
+    </div>
+    <div class="sub_box1">
+    <?php $image = get_field('section_4_box_2_image');?>
+      <img src="<?php echo $image;?>" alt="">
+      <h4><?php echo get_field('section_4_box_2_title'); ?></h4>
+      <p><?php echo get_field('section_4_box_2_description'); ?></p>
+    </div>
+    <div class="sub_box1">
+    <?php $image = get_field('section_4_box_3_image');?>
+      <img src="<?php echo $image;?>" alt="">
+      <h4><?php echo get_field('section_4_box_3_title'); ?></h4>
+      <p><?php echo get_field('section_4_box_3_description'); ?></p>
+    </div>
+  </div> 
+</section>
 
 
 
