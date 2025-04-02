d0e1ee2da7213140d5a447f48d113ee02e3494d9
shohel931 <shohel065931@gmail.com>
2025-04-02 11:03:44 +0600

    responsiv

--- inc/enqueue_register.php
+++ inc/enqueue_register.php
@@ -5,19 +5,24 @@ function css_js_fils_enqueue(){
   wp_register_style('bxslider', get_template_directory_uri(  ). '/css/bxslider.min.css', array(), '4.2.17', 'all');
   wp_register_style('bootstrap', get_template_directory_uri(  ). '/css/bootstrap.css', array(), '5.0.2', 'all');
   wp_register_style('custom', get_template_directory_uri(  ). '/css/custom.css', array(), '1.0.0', 'all');
+  wp_register_style('slicknav', get_template_directory_uri(  ). '/css/slicknav.css', array(), '1.0.1', 'all');
   wp_register_style('page_style', get_template_directory_uri(  ). '/css/page_style.css', array(), '1.0.0', 'all');
   wp_register_style('services_page', get_template_directory_uri(  ). '/css/services_page.css', array(), '1.0.0', 'all');
+  wp_register_style('responsive', get_template_directory_uri(  ). '/css/responsive.css', array(), '1.0.0', 'all');
   wp_enqueue_style('bxslider');
   wp_enqueue_style('bootstrap');
   wp_enqueue_style('custom');
+  wp_enqueue_style('slicknav');
   wp_enqueue_style('page_style');
   wp_enqueue_style('services_page');
+  wp_enqueue_style('responsive');
 
 //js
 wp_enqueue_script('jquery');
 wp_enqueue_script('bxslider.min', get_template_directory_uri(  ).'/js/bxslider.min.js', array(), '4.2.17', true);
 wp_enqueue_script('bootstrap', get_template_directory_uri(  ).'/js/bootstrap.js', array(), '5.0.2', true);
 wp_enqueue_script('main', get_template_directory_uri(  ).'/js/main.js', array(), '1.0.0', true);
+wp_enqueue_script('slicknav', get_template_directory_uri(  ).'/js/slicknav.js', array(), '1.0.1', true);
 
 
 
--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 // Bxslider
 jQuery(document).ready(function(){
+  jQuery('#nav').slicknav();
   jQuery(".slider").bxSlider({
     mode: 'fade',
     captions: true,
